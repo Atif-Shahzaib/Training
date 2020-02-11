c58be44502751cd82de786c0e9477f0b509b5872
YOUR NAME <YOUR@EMAIL.com>
2020-02-11 13:25:57 +0500

    Validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+	attr_accessor :title, :body
+	validates :title, :body, :presence => true
+	validates :title, :length => { :minimum => 2}
+	validates :title, :uniqueness => true
 end
